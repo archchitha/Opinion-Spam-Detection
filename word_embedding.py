28ffc24a5e3872cc2396093a5cd8e01b9288c6c4
archchitha <archchithak@gmail.com>
2019-01-02 21:33:15 +0530

    Add files via upload

---
+++ word_embedding.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+df_data = pd.read_csv('data/deceptive-opinion.csv', sep=',', names=['label', 'hotel', 'polarity', 'source', 'review_text'])
+from sklearn.model_selection import train_test_split
+review = df_data['review_text'].values
+y = df_data['label'].values
+review_train, review_test, y_train, y_test = train_test_split(review, y, test_size=0.25, random_state=1000)
+
+
+
+from keras.preprocessing.text import Tokenizer
+
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(review_train)
+review_train = tokenizer.texts_to_sequences(review_train)
+review_test = tokenizer.texts_to_sequences(review_test)
+vocab_size = len(tokenizer.word_index) + 1
+
+from keras.preprocessing.sequence import pad_sequences
+
+maxlen = 100
+
+X_train = pad_sequences(review_train, padding='post', maxlen=maxlen)
+X_test = pad_sequences(review_test, padding='post', maxlen=maxlen)
+
+
+
+from keras.models import Sequential
+from keras import layers
+
+embedding_dim = 50
+
+model = Sequential()
+
+model.add(layers.Embedding(input_dim=vocab_size,
+                           output_dim=embedding_dim,
+                           input_length=maxlen))
+model.add(layers.GlobalMaxPool1D())
+model.add(layers.Dense(10, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.summary()
+
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+
+
+def plot_history(history):
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, acc, 'b', label='Training acc')
+    plt.plot(x, val_acc, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Training loss')
+    plt.plot(x, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
+    plt.show()
+
+
+history = model.fit(X_train, y_train,
+                    epochs=20,
+                    verbose=False,
+                    validation_data=(X_test, y_test),
+                    batch_size=10)
+loss, accuracy = model.evaluate(X_train, y_train, verbose=False)
+print("Training Accuracy: {:.4f}".format(accuracy))
+loss, accuracy = model.evaluate(X_test, y_test, verbose=False)
+print("Testing Accuracy:  {:.4f}".format(accuracy))
+plot_history(history)
+
+
+
